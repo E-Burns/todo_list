74502bf16ed3c582910ee49ab1a7e9bba8c27110
Eowyn <eowynburns@gmail.com>
2022-08-10 10:03:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    //tasks go in here
+    { name: "Eat Jaffa cakes", priority: "high", isDone: false },
+    { name: "Make Herbert a website", priority: "medium", isDone: false },
+    { name: "Watch Empire Strikes Back", priority: "low", isDone: false }
+  ]);
+
+  const [newTask, setNewTask] = useState('');
+  const [newTaskPriority, setNewPriority] = useState('');
+
+
+  //making the list
+
+  const taskNodes = tasks.map((task, index) => {
+    return(
+      <li key={index} className={
+        task.isDone === true ? 'done' :
+        task.priority === 'high' ? 'high' : task.priority === 'medium' ? 'medium' : 'low'}>
+        <span>{task.name}</span>
+
+        {task.isDone ? <span className='done'>Task completed!</span> : <button onClick={() => changeIsDone(index)}>Done?</button>}
+      </li>
+    );
+  });
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const handleTaskPriority = (event) => {
+    setNewPriority(event.target.value);
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    if(newTask.length > 0 && (newTaskPriority == 'high' || newTaskPriority == 'medium' || newTaskPriority == 'low')){
+      const copyTasks = [...tasks];
+      copyTasks.push({name: newTask, priority: newTaskPriority, isDone: false});
+      setTasks(copyTasks);
+      setNewTask('');
+    }
+  }
+
+  const changeIsDone = (index) => {
+    const copyTasks = [...tasks];
+    copyTasks[index].isDone = true;
+    setTasks(copyTasks);
+  }
+
+
+  //editing the list
+
+  const handleRemoveAll = (event) => {
+    event.preventDefault();
+    const emptyArray = [];
+    setTasks(emptyArray);
+  }
+
+  const handleRemoveDone = (event) => {
+    event.preventDefault();
+    const filteredTasks = tasks.filter(task => task.isDone === false);
+    setTasks(filteredTasks);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>To-Do List</h1>
+      <hr></hr>
+
+{/* making list */}
+      <form onSubmit={saveNewTask}>
+        <input id='new-task' type='text' value={newTask} onChange={handleTaskInput} />
+        
+        <input type='radio' id='priorityHigh' name="priority" value='high' onChange={handleTaskPriority} />
+          <label htmlFor='priorityHigh'>High</label>
+
+        <input type='radio' id='priorityMed' name="priority" value='medium' onChange={handleTaskPriority} />
+          <label htmlFor='priorityMed'>Medium</label>
+
+        <input type='radio' id='priorityLow' name="priority" value='low' onChange={handleTaskPriority} />
+          <label htmlFor='priorityLow'>Low</label>
+
+        <input type='submit' value='Save Task'/>
+      </form>
+
+{/* editing list */}
+      <form>
+        <button onClick={handleRemoveAll}>Remove all</button>
+        <button onClick={handleRemoveDone}>Remove done</button>
+      </form>
+
+{/* list */}
+      <ul>
+        {taskNodes}
+      </ul>
+
     </div>
   );
 }
