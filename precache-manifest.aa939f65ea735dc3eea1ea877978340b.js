8fc17b53eddd9428f677aed7459e6557fb779c86
Eowyn <eowynburns@gmail.com>
2022-08-23 10:41:27 +0100

    Updates

---
+++ precache-manifest.aa939f65ea735dc3eea1ea877978340b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62058892e70694663bc1",
+    "url": "/E-Burns/todo_list.git/static/css/main.09be3b85.chunk.css"
+  },
+  {
+    "revision": "62058892e70694663bc1",
+    "url": "/E-Burns/todo_list.git/static/js/main.62058892.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/E-Burns/todo_list.git/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "199af2c2a0c941a3b7e7",
+    "url": "/E-Burns/todo_list.git/static/js/2.199af2c2.chunk.js"
+  },
+  {
+    "revision": "29aef8c5ce3614013af8",
+    "url": "/E-Burns/todo_list.git/static/js/runtime~main.29aef8c5.js"
+  },
+  {
+    "revision": "ab6b78d82d8dbbb02ccf883fa71096e7",
+    "url": "/E-Burns/todo_list.git/index.html"
+  }
+];
